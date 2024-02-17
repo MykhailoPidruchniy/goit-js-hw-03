a0e490d46930c8eb007e26693376bd0accd2bd68
MykhailoPidruchniy <gitomir17@gmail.com>
2024-02-17 13:50:27 +0200

    goit-js-hw-03

--- js/task-1.js
+++ js/task-1.js
@@ -1,17 +1,10 @@
-function makeTransaction(pricePerDroid, orderedQuantity, customerCredits) {
-  let message;
-const totalPrice = pricePerDroid * orderedQuantity;
-  if (totalPrice <= customerCredits) {
-       message = `You ordered ${pricePerDroid} droids worth ${totalPrice} credits!`;  
-  } else {
- message = 'Insufficient funds!';
-  }
+function slugify(title) {
+  let titleNew = title.split(" ").join("-").toLowerCase();
   
-  return message;
+  return titleNew
 }
 
-console.log(makeTransaction(5, 3000, 23000)); // "You ordered 5 droids worth 15000 credits!"
-console.log(makeTransaction(3, 1000, 15000)); // "You ordered 3 droids worth 3000 credits!"
-console.log(makeTransaction(10, 5000, 8000)); // "Insufficient funds!"
-console.log(makeTransaction(8, 2000, 10000)); // "Insufficient funds!"
-console.log(makeTransaction(10, 500, 5000)); // "You ordered 10 droids worth 5000 credits!"
+console.log(slugify("Arrays for begginers")); // "arrays-for-begginers"
+console.log(slugify("English for developer")); // "english-for-developer"
+console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
